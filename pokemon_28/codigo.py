668f9dd4fac5650f20a9f8cc93ef82043b0176c6
paulaanb <pnarabor@myuax.com>
2022-10-26 17:29:11 +0200

    calculamos el poder de ataque

--- pokemon_28/codigo.py
+++ pokemon_28/codigo.py
@@ -40,3 +40,24 @@ def obtener_diccionario_segun_csv():
                 "movimientos_posibles": movimientos_posibles,
             }
     return pokemones
+
+#Calculamos el poder de ataque
+from moves import get_move
+
+def solicitar_movimiento(movimientos):
+    while True:
+        print("Movimientos que puede aprender el pokémon: ")
+        for indice, movimiento in enumerate(movimientos):
+            print(f"{indice} - {movimiento}")
+        indice_movimiento = int(input("Seleccione un ataque a ejecutar: "))
+        if indice_movimiento < 0 or indice_movimiento > len(movimientos) - 1:
+            print("Número inválido")
+            continue
+
+        movimiento = movimientos[indice_movimiento]
+        print(f"El ataque seleccionado es: {movimiento}")
+        poder_ataque = get_move(normalizar_nombre(movimiento))
+        if poder_ataque > 0:
+            return poder_ataque
+        else:
+            print("El ataque tiene un poder de 0. Seleccione otro movimiento")
